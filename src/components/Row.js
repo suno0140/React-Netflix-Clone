5caaf347359052f27681668cc761e3a4fa0e920f
suno0140 <vlfxhd15@naver.com>
2023-04-17 14:43:38 +0900

    feat: movie Row 생성

---
+++ src/components/Row.js
@@ -0,0 +1,46 @@
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+const Row = ({ isLargeRow, title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+  };
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+      </div>
+      <div id={id} className="row__posterssetMovies">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`https://image.tmdb.org/t/p/original/${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            loading="lazy"
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      <div className="slider__arrow-rigt">
+        <span className="arrow">{">"}</span>
+      </div>
+    </section>
+  );
+};
+
+export default Row;
